536170d4a153f07fe7fe4f81ffe9d8069bf5099a
paulshearer <pshearer@hotmail.com>
2023-10-29 11:28:49 -0400

    create_df_from_def

--- dataframes/create_df_from_def.r
+++ dataframes/create_df_from_def.r
@@ -1,10 +1,13 @@
-#create a DataFrame from a csv definition
+#create a Data Frame from a csv definition
 #Paul Shearer 10-29-2003
 #
 #
 #sampleDF <- create_df_from_def("<path>//df_example_def.csv")
 #see df_def_example.csv in this repo for example of the def file 
 #
+# Note that functionality for Allowed_Values has not been implemented 
+#
+
 
 create_df_from_def <- function(dfDef){
   myDFdef <- read.csv(dfDef, header = TRUE,fileEncoding = "UTF-8-BOM")
