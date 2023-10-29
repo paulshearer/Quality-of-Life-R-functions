408a7bc3e9624de3b892b8fcdfef4c97cb918d4a
paulshearer <pshearer@hotmail.com>
2023-10-29 11:16:19 -0400

    Create create_df_from_def.r

---
+++ create_df_from_def.r
@@ -0,0 +1,17 @@
+#create a DataFrame from a csv definition
+#Paul Shearer 10-29-2003
+#
+#
+#sampleDF <- create_df_from_def("<path>//df_example_def.csv")
+#see df_def_example.csv in this repo for example of the def file 
+#
+
+create_df_from_def <- function(dfDef){
+  myDFdef <- read.csv(dfDef, header = TRUE,fileEncoding = "UTF-8-BOM")
+  myDFdef <- str_c(myDFdef$column_name,"=",myDFdef$data_type,",")
+  myDFdef <- paste(myDFdef, collapse = '')
+  myDFdef <- str_sub(myDFdef,1,nchar(myDFdef)-1)
+  myDFdef <- paste0("data.frame(",myDFdef,")")
+  myDF <- eval(parse(text=paste0(myDFdef)))
+  return(myDF)
+}
